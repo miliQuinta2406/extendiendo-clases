9f1ffe0c9aacbce26ee80faca49d5be94423afe5
Milagros Quintana <milagrosquintana735@gmail.com>
2026-05-24 17:35:06 -0300

    implemento ListaDeProuctosss

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+import products from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
+
   constructor(name: string) {
-    // nombre de esta lista
     this.name = name;
   }
-  add(nuevaCosa) {
+
+  add(nuevaCosa: any) {
     this.cosas.push(nuevaCosa);
   }
+
   getCosas() {
     return this.cosas;
   }
@@ -17,6 +21,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +29,44 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string){
+    super(name);
+
+    products.forEach((product) =>{
+      this.addProduct(product);
+    });
+  }
+addProduct(product: Product): void {
+  const productExists = this.getProduct(product.id);
+
+  if(!productExists){
+    this.add(product);
+  }
+}
+
+getProduct(id: number): Product {
+  return this.cosas.find((product) => product.id === id);
+}
+
+removeProduct(id: number): void {
+  this.cosas = this.cosas.filter((product) => product.id !== id);
+}
+
+getSortedByPrice(order: string): Product [] {
+  const productsCopy = [...this.cosas];
+
+  if (order === "asc"){
+    return productsCopy.sort((a, b) => a.price - b.price);
+
+  }
+
+  if (order == "desc"){
+    return productsCopy.sort((a, b) => b.price - a.price);
+  }
+
+  return productsCopy;
+}
+}
 
 export { ListaDeProductos, Product };
